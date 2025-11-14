40d8eb48e755cb666ba4ef6d1049944b3a1c4f88
Chizkiyahu Raful <chizkiyahuraful@gmail.com>
2025-11-14 06:07:18 +0200

    remove vulkan-validationlayers
    Signed-off-by: Chizkiyahu Raful <chizkiyahuraful@gmail.com>

--- .github/workflows/scripts/build_lavapipe_macos.zsh
+++ .github/workflows/scripts/build_lavapipe_macos.zsh
@@ -70,7 +70,7 @@ brew_install_if_needed llvm
 brew_install_if_needed libclc
 brew_install_if_needed vulkan-loader
 brew_install_if_needed vulkan-tools
-brew_install_if_needed vulkan-validationlayers
+#brew_install_if_needed vulkan-validationlayers
 
 # Make sure llvm-config and llvm-ar are on PATH (llvm is keg-only in Homebrew)
 LLVM_PREFIX="$(brew --prefix llvm 2>/dev/null || true)"
